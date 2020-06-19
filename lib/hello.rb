785b203b8a5d7224afe735350c2ca95d3b8efbf2
Nick Lanese <nlanese21@gmail.com>
2020-06-19 18:39:44 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,10 +1,8 @@
 def hello_t(array)
-  array.each do | name |
-    if name.start_with?("T")
-      puts "Hi, #{name}"
-    end
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i = i + 1
   end
-  return array
+  array
 end
-
-# call your method here!
